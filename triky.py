6300aa5a78362de2a4defefb10939d54c05db35e
domainKT01 <luisdanielcoronelposada903@gmail.com>
2022-06-20 17:00:14 -0500

    final

--- triky.py
+++ triky.py
@@ -104,112 +104,109 @@ def huboGanador():
 def controlLinea():
     global ganador
     global gano
-    if (varmatriz[0][0] == varmatriz[0][1] == varmatriz[0][2] != "-" or tablero2[0][1] == tablero2[0][2] == tablero2[0][3] != "-") :
+    if (varmatriz[0][0] == varmatriz[0][1] == varmatriz[0][2] != "-" ) :
         gano = "gano horizontal"
-        if (varmatriz[0][0]):
-            ganador = varmatriz[0][0]
-            return gano
-        elif (tablero2[0][1]):
-            ganador = tablero2[0][1]
-            return gano
-    elif (varmatriz[1][0] == varmatriz[1][1] == varmatriz[1][2] != "-" or tablero2[1][1] == tablero2[1][2] == tablero2[1][3] != "-") :
+        ganador = varmatriz[1][0]
+        return gano
+    elif (tablero2[0][1] == tablero2[0][2] == tablero2[0][3] != "-") :
         gano = "gano horizontal"
-        if (varmatriz[1][0]):
-            ganador = varmatriz[1][0]
-            return gano
-        elif (tablero2[1][1]):
-            ganador = tablero2[1][1]
-            return gano
-    elif (varmatriz[2][0] == varmatriz[2][1] == varmatriz[2][2] != "-" or tablero2[2][1] == tablero2[2][2] == tablero2[2][3] != "-") :
+        ganador = tablero2[0][1]
+        return gano
+    elif (varmatriz[1][0] == varmatriz[1][1] == varmatriz[1][2] != "-" ) :
         gano = "gano horizontal"
-        if (varmatriz[2][0]):
-            ganador = varmatriz[2][0]
-            return gano
-        elif (tablero2[2][1]):
-            ganador = tablero2[2][1]
-            return gano
+        ganador = varmatriz[1][0]
+        return gano
+    elif (tablero2[1][1] == tablero2[1][2] == tablero2[1][3] != "-") :
+        gano = "gano horizontal"
+        ganador = tablero2[1][1]
+        return gano
+    elif (varmatriz[2][0] == varmatriz[2][1] == varmatriz[2][2] != "-" ) :
+        gano = "gano horizontal"
+        ganador = varmatriz[2][0]
+        return gano
+    elif (tablero2[2][1] == tablero2[2][2] == tablero2[2][3] != "-") :
+        gano = "gano horizontal"
+        ganador = tablero2[2][1]
+        return gano
+        
 
 ###################################################################
 
 
 def controlVertical():
     global ganador
-    if (varmatriz[0][0] == varmatriz[1][0] == varmatriz[2][0] != "-" or tablero2[1][0] == tablero2[2][0] == tablero2[3][0] != "-"):
+    if (varmatriz[0][0] == varmatriz[1][0] == varmatriz[2][0] != "-" ):
         gano = "gano vertical"
-        if (varmatriz[0][0]) :
-            ganador = varmatriz[0][0]
-            return gano
-        elif (tablero2[1][0]) :
-            ganador = tablero2[1][0]
-            return gano
-    elif (varmatriz[0][1] == varmatriz[1][1] == varmatriz[2][1] != "-" 
-        or tablero2[1][1] == tablero2[2][1] == tablero2[3][1] != "-") :
+        ganador = varmatriz[0][0]
+        return gano
+    elif (tablero2[1][0] == tablero2[2][0] == tablero2[3][0] != "-"):
         gano = "gano vertical"
-        if (varmatriz[0][1]) :
-            ganador = varmatriz[0][1]
-            return gano
-        elif (tablero2[1][0]) :
-            ganador = tablero2[1][0]
-            return gano
-    elif (varmatriz[0][2] == varmatriz[1][2] == varmatriz[2][2] != "-" 
-        or tablero2[1][2] == tablero2[2][2] == tablero2[3][2] != "-") :
+        ganador = tablero2[1][0]
+        return gano
+    elif (varmatriz[0][1] == varmatriz[1][1] == varmatriz[2][1] != "-" ) :
         gano = "gano vertical"
-        if (varmatriz[0][2]) :
-            ganador = varmatriz[0][2]
-            return gano
-        elif (tablero2[1][2]) :
-            ganador = tablero2[1][2]
-            return gano
-    elif (varmatriz[0][3] == varmatriz[1][3] == varmatriz[2][3] != "-" 
-        or tablero2[1][3] == tablero2[2][3] == tablero2[3][3] != "-"):
+        ganador = varmatriz[0][1]
+        return gano
+    elif (tablero2[1][1] == tablero2[2][1] == tablero2[3][1] != "-"):
         gano = "gano vertical"
-        if (varmatriz[0][3]) :
-            ganador = varmatriz[0][3]
-            return gano
-        elif (tablero2[1][3]) :
-            ganador = tablero2[1][3]
-            return gano
+        ganador = tablero2[1][1]
+        return gano
+    elif (varmatriz[0][2] == varmatriz[1][2] == varmatriz[2][2] != "-" ) :
+        gano = "gano vertical"
+        ganador = varmatriz[0][2]
+        return gano
+    elif (tablero2[1][2] == tablero2[2][2] == tablero2[3][2] != "-"):
+        gano = "gano vertical"
+        ganador = tablero2[1][2]
+        return gano
+    elif (varmatriz[0][3] == varmatriz[1][3] == varmatriz[2][3] != "-" ):
+        gano = "gano vertical"
+        ganador = varmatriz[0][3]
+        return gano
+    elif (tablero2[1][3] == tablero2[2][3] == tablero2[3][3] != "-"):
+        gano = "gano vertical"
+        ganador = tablero2[1][3]
+        return gano
 
 ###################################################################
 
 
 def controlDiagonal():
     global ganador
-    if (varmatriz[0][0] == varmatriz[1][1] == varmatriz[2][2] != "-"
-        or tablero2[1][0] == tablero2[2][1] == tablero2[3][2] != "-"
-        or tablero2[0][1] == tablero2[1][2] == tablero2[2][3] != "-"
-        or tablero2[1][1] == tablero2[2][2] == tablero2[3][3] != "-" ):
+    if (varmatriz[0][0] == varmatriz[1][1] == varmatriz[2][2] != "-" ) :
         gano = "gano diagonal"
-        if (varmatriz[0][0]) :
-            ganador = varmatriz[0][0]
-            return gano
-        elif (varmatriz[0][1]) :
-            ganador = varmatriz[0][1]
-            return gano
-        elif (varmatriz[0][1]) :
-            ganador = varmatriz[0][1]
-            return gano
-        elif (varmatriz[1][1]) :
-            ganador = varmatriz[1][1]
-            return gano
-    elif (varmatriz[0][2] == varmatriz[1][1] == varmatriz[2][0] != "-"
-        or tablero2[0][3] == tablero2[1][2] == tablero2[2][1] != "-"
-        or tablero2[1][3] == tablero2[2][2] == tablero2[3][2] != "-"
-        or tablero2[1][2] == tablero2[2][1] == tablero2[3][0] != "-") :
+        ganador = varmatriz[0][0]
+        return gano
+    elif (tablero2[1][0] == tablero2[2][1] == tablero2[3][2] != "-") :
         gano = "gano diagonal"
-        if (varmatriz[0][2]) :
-            ganador = varmatriz[0][2]
-            return gano
-        elif (varmatriz[0][3]) :
-            ganador = varmatriz[0][3]
-            return gano
-        elif (varmatriz[1][3]) :
-            ganador = varmatriz[1][3]
-            return gano
-        elif (varmatriz[1][2]) :
-            ganador = varmatriz[1][2]
-            return gano
-
+        ganador = tablero2[1][0]
+        return gano
+    elif (tablero2[0][1] == tablero2[1][2] == tablero2[2][3] != "-") :
+        gano = "gano diagonal"
+        ganador = tablero2[0][1]
+        return gano
+    elif (tablero2[1][1] == tablero2[2][2] == tablero2[3][3] != "-") :
+        gano = "gano diagonal"
+        ganador = tablero2[1][1]
+        return gano
+    elif (varmatriz[0][2] == varmatriz[1][1] == varmatriz[2][0] != "-" ) :
+        gano = "gano diagonal"
+        ganador = varmatriz[0][2]
+        return gano
+    elif (tablero2[0][3] == tablero2[1][2] == tablero2[2][1] != "-") :
+        gano = "gano diagonal"
+        ganador = tablero2[0][3]
+        return gano
+    elif (tablero2[1][3] == tablero2[2][2] == tablero2[3][2] != "-") :
+        gano = "gano diagonal"
+        ganador = tablero2[1][3]
+        return gano
+    elif (tablero2[1][2] == tablero2[2][1] == tablero2[3][0] != "-") :
+        gano = "gano diagonal"
+        ganador = tablero2[1][2]
+        return gano
+        
+        
 ###################################################################
 
 
