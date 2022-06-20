7a452165cce0f4288cf64f33c842bd2943e6f4e5
domainKT01 <luisdanielcoronelposada903@gmail.com>
2022-06-20 16:07:04 -0500

    step 12

--- triky.py
+++ triky.py
@@ -104,29 +104,37 @@ def huboGanador():
 def controlLinea():
     global ganador
     global gano
-    if (varmatriz[0][0] == varmatriz[0][1] == varmatriz[0][2] != "-"
-        or tablero2[0][1] == tablero2[0][2] == tablero2[0][3] != "-") :
-        ganador = tablero[0][1]
+    if (varmatriz[0][0] == varmatriz[0][1] == varmatriz[0][2] != "-" or tablero2[0][1] == tablero2[0][2] == tablero2[0][3] != "-") :
         gano = "gano horizontal"
-        return gano
-    elif (varmatriz[1][0] == varmatriz[1][1] == varmatriz[1][2] != "-"
-        or varmatriz[1][1] == varmatriz[1][2] == varmatriz[1][3] != "-") :
-        ganador = tablero[1][1]
+        if (varmatriz[0][0]):
+            ganador = varmatriz[0][0]
+            return gano
+        elif (tablero2[0][1]):
+            ganador = tablero2[0][1]
+            return gano
+    elif (varmatriz[1][0] == varmatriz[1][1] == varmatriz[1][2] != "-" or tablero2[1][1] == tablero2[1][2] == tablero2[1][3] != "-") :
         gano = "gano horizontal"
-        return gano
-    elif (varmatriz[2][0] == varmatriz[2][1] == varmatriz[2][2] != "-"
-        or tablero2[2][1] == tablero2[2][2] == tablero2[2][3] != "-") :
-        ganador = tablero[2][1]
+        if (varmatriz[1][0]):
+            ganador = varmatriz[1][0]
+            return gano
+        elif (tablero2[1][1]):
+            ganador = tablero2[1][1]
+            return gano
+    elif (varmatriz[2][0] == varmatriz[2][1] == varmatriz[2][2] != "-" or tablero2[2][1] == tablero2[2][2] == tablero2[2][3] != "-") :
         gano = "gano horizontal"
-        return gano
+        if (varmatriz[2][0]):
+            ganador = varmatriz[2][0]
+            return gano
+        elif (tablero2[2][1]):
+            ganador = tablero2[2][1]
+            return gano
 
 ###################################################################
 
 
 def controlVertical():
     global ganador
-    if (varmatriz[0][0] == varmatriz[1][0] == varmatriz[2][0] != "-" 
-        or tablero2[1][0] == tablero2[2][0] == tablero2[3][0] != "-"):
+    if (varmatriz[0][0] == varmatriz[1][0] == varmatriz[2][0] != "-" or tablero2[1][0] == tablero2[2][0] == tablero2[3][0] != "-"):
         gano = "gano vertical"
         if (varmatriz[0][0]) :
             ganador = varmatriz[0][0]
